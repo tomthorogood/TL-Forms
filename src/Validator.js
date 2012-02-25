71b6f2a87251267a0a084aaba770bd0e7921820e
Tom Thorogood <tom@tomthorogood.com>
2012-02-25 13:32:43 -0500

    Rewrote Validator to work with new style Element

--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,10 @@
 /*.............................................
 The validator() class allows live field validation.
 */
-function Validator (against, /*optional => */delay, valid_css, invalid_css)
+function Validator (against, /*optional => */delay, animation_speed, valid_css, invalid_css)
 /* against needs to be a function that takes a value, and returns a value in one of the following formats:
  * bool (true/false), array [bool, message]
- * delay defaults to 650ms, but can be set by passing something in here.
+ * DELAY defaults to 650ms, but can be set by passing something in here.
  * valid_css defaults to a teal background with white text, and represents the appearance of a form field when 
  * it has been validated. It can be set here.
  * invalid_css defaults to a red background with white text, and represents the appearance of a form field
@@ -15,7 +15,8 @@ function Validator (against, /*optional => */delay, valid_css, invalid_css)
                                    });
 */
 {
-    this.delay = delay || 650;
+    this.ANIMATION_SPEED = animation_speed || 250;
+    this.DELAY = delay || 650;
     this.css = {
         valid   :   valid_css || {"background-color" : "#00afad","color" : "#fff"},
         invalid :   invalid_css || {"background-color" : "#811", "color" : "#fff"}
@@ -23,60 +24,60 @@ function Validator (against, /*optional => */delay, valid_css, invalid_css)
     this.test = against;
     this.set_text = {};
     this.validate = function (element)
-    { // Validates fields after an x ms delay, where x is this.delay; 
+    { // Validates fields after an x ms DELAY, where x is this.DELAY; 
       // after testing, animates the field to the valid or invalid css.
-        var _self = this;
+        var _self_ = this;
         var timer;
         var valid;
         if (element.type.toLowerCase() === "radio")
         {
-            $(element).change(function() {
-                element = typeof element === "string" ? $(element) : element;
-                valid = _self.test(element.value);
-                valid = typeof valid === "object" ? valid[0] : valid; 
-                element.valid = valid;
-                var c = element.valid === true ? _self.css.valid : _self.css.invalid;
-                $(element).css(c);
-            });
+            for (var i = 0; i < element.input.length; i++)
+            {
+                $(element.input[i]).change(function() {
+                    return function() {
+                        var value = this.value;
+                        valid = _self_.test(value);
+                        var css = valid ? _self_.css.valid : _self_.css.invalid;
+                        element.valid = valid;
+                        $(element).animate(css,_self_.ANIMATION_SPEED);
+                    };
+                });
+            }
         }
         else
         {
             $(element).keyup(function() {
-                $(element).keydown(function(event) {
+                $(this).keydown(function(event) {
                     if (event.keyCode !== 9)
-                    {
+                    {//Unless the user is tabbing out the field, reset so that we don't annoy them while they're trying to type
                         clearTimeout(timer);
                     }
                 });
                 if (typeof element.value !== 'undefined' && element.value.length > 0)
                 {
                     timer = setTimeout(function() {
-                        valid = _self.test(element.value);
+                        valid = _self_.test(element.value);
                         switch(typeof valid)
-                        {
-                            case "object"   :   _self.set_text[element.name] = valid[1].toString();
+                        {//If an object is returned, we parse the first value of the array as true/false, and the second 
+                            //as feedback to the user.
+                            case "object"   :   _self_.set_text[element.name] = valid[1].toString();
                                                 valid = valid[0];
                                                 //Do not put a break here! This cascades on purpose! 
-                            case "boolean"  :   var css = valid ? _self.css.valid : _self.css.invalid;
+                            case "boolean"  :   var css = valid ? _self_.css.valid : _self_.css.invalid;
                                                 element.valid = valid;
                                                 break;
                             default         :   valid = true; //Don't punish the user if the programmer doesn't know what they're doing!
                                                 element.valid = valid;
                         }
-                        switch(typeof element)
-                        {
-                            case "string"   :   element = element[0] === "#" ? element : "#"+element;
-                            case "object"   :   $(element).animate(css, 250, function() {
-                                                    if (typeof _self.set_text[element.name] === "string")
-                                                    {
-                                                        $(element).val(_self.set_text[element.name]);
-                                                        delete _self.set_text[element.name];
-                                                    }
-                                                });
-                                                return;
-                            case "undefined" :  return valid;
-                        }
-                    }, _self.delay);
+                        $(element.input[0]).animate(css,_self_.ANIMATION_SPEED,function() {
+                            if (typeof _self_.set_text[element.name] === "string")
+                            {//Change the text if there has been alternate text provided
+                                element.input[0].value = _self_.set_text[element.name];
+                                delete _self_.set_text[element.name];
+                            }
+                        });
+                                
+                    }, _self_.DELAY);
                 }
             });
         }
