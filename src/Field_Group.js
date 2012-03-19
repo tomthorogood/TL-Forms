87ef5ab302e093a9e88d2625500ea5cd3dba58fb
Tom Thorogood <tom@tomthorogood.com>
2012-03-19 14:42:28 -0400

    Add method in Field_Group for adding text in a more OO way.

--- src/Field_Group.js
+++ src/Field_Group.js
@@ -19,3 +19,35 @@ function Field_Group (name, array)
     }
 }
 
+Field_Group.prototype.add_text = function (text, where, css)
+{
+    var div = document.createElement('div');
+    if (typeof css === "string")
+    {
+        $(div).addClass(css);
+    }
+    else if (typeof css === "object")
+    {
+        $(div).css(css);
+    }
+    div.innerHTML = text;
+
+    switch(where)
+    {
+        case "before"   :   $(this.div).prepend(div);
+                            break;
+        case "after"    :   $(this.div).append(div);
+                            break;
+    }
+}
+
+Field_Group.prototype.prepend_text = function (text, css)
+{
+    this.add_text(text, "before", css);
+}
+
+Field_Group.prototype.append_text = function (text,css)
+{
+    this.add_text(text, "after", css);
+}
+
